fcaeb9a7677616da8115197a42d219d95b1750a0
Evan Reichard <evan@evanreichard.com>
2013-12-03 06:21:50 -0500

    Guest GUI Completed
    Guest GUI completed

--- GuestGUI.java
+++ GuestGUI.java
@@ -10,17 +10,10 @@ public class GuestGUI implements ActionListener{
 	
 	JTextArea reportTextArea;
 	
-	JTextField classIDField;
-	JTextField classNameField;
-	JTextField classCreditField;
-	JTextField classRequirementsField;
-	
 	JComboBox majorList;
 	JComboBox minorList;
 	
 	JButton jbGenReport;
-	JButton jbUpdateAddClass;
-	JButton jbDeleteClass;
 	
 	public GuestGUI(){
 	
@@ -35,18 +28,10 @@ public class GuestGUI implements ActionListener{
 		JPanel leftP = new JPanel(new GridLayout(2,0));
 		JPanel rightP = new JPanel();
 		JPanel leftTopP = new JPanel();
-		JPanel leftBottomP = new JPanel();
 		
-		JPanel classIDPanel = new JPanel(new GridLayout(0,2));
-		JPanel classNamePanel = new JPanel(new GridLayout(0,2));
-		JPanel classCreditPanel = new JPanel(new GridLayout(0,2));
-		JPanel classRequirementPanel = new JPanel(new GridLayout(0,2));
-		JPanel classButtonPanel = new JPanel();
 		
 		// Buttons
 		jbGenReport = new JButton("Generate Report");
-		jbUpdateAddClass = new JButton("Update / Add Class");
-		jbDeleteClass = new JButton("Delete Class");
 		
 		// TextArea w/ Scroll
 		reportTextArea = new JTextArea(28, 30);
@@ -56,35 +41,17 @@ public class GuestGUI implements ActionListener{
 		// JComboBox
 		String[] tempMajors = {"Select Major"};
 		String[] tempMinors = {"Select Minor"};
-		String[] tempClassID = {"Select Class"};
 		
 		majorList = new JComboBox(tempMajors);
 		minorList = new JComboBox(tempMinors);
 		majorList.setPreferredSize(new Dimension(320, 30));
 		minorList.setPreferredSize(new Dimension(320, 30));
 		
-		majorList.setEnabled(false);
-		minorList.setEnabled(false);
-		
-		// JLabels
-		JLabel classIDLabel = new JLabel("ID:");
-		JLabel classNameLabel = new JLabel("Name:");
-		JLabel classCreditLabel = new JLabel("Credit Value:");
-		JLabel classRequirementsLabel = new JLabel("Requirements:");
-		
-		// JTextFields
-		classIDField = new JTextField(15);
-		classNameField = new JTextField(15);
-		classCreditField = new JTextField(15);
-		classRequirementsField = new JTextField(15);
-		
 		// Populate before ActionListener
 		populateComboBoxes();
 		
 		// Action Listener
 		jbGenReport.addActionListener(this);
-		jbUpdateAddClass.addActionListener(this);
-		jbDeleteClass.addActionListener(this);
 		majorList.addActionListener(this);
 		minorList.addActionListener(this);
 		
@@ -96,29 +63,8 @@ public class GuestGUI implements ActionListener{
 		
 		// leftTopP.add(classList);
 		//leftTopP.add(Box.createRigidArea(new Dimension(0,200)));
-		classIDPanel.add(classIDLabel);
-		classIDPanel.add(classIDField);
-		
-		classNamePanel.add(classNameLabel);
-		classNamePanel.add(classNameField);
-		
-		classCreditPanel.add(classCreditLabel);
-		classCreditPanel.add(classCreditField);
-		
-		classRequirementPanel.add(classRequirementsLabel);
-		classRequirementPanel.add(classRequirementsField);
-		
-		classButtonPanel.add(jbUpdateAddClass);
-		classButtonPanel.add(jbDeleteClass);
-		
-		leftBottomP.add(classIDPanel);
-		leftBottomP.add(classNamePanel);
-		leftBottomP.add(classCreditPanel);
-		leftBottomP.add(classRequirementPanel);
-		leftBottomP.add(classButtonPanel);
 		
 		leftP.add(leftTopP);
-		leftP.add(leftBottomP);
 		rightP.add(myScrollReport);
 		
 		myGuestGUI.add(leftP);
@@ -132,16 +78,26 @@ public class GuestGUI implements ActionListener{
 		ArrayList<String> tempMajAL;
 		ArrayList<String> tempMinAL;
 		
-		classIDField.setText("");
-		classNameField.setText("");
-		classCreditField.setText("");
-		classRequirementsField.setText("");
+		// On load, populate Major, Minor, and Class ComboBoxes
+		ArrayList<ArrayList> myMajors = DBClassAccessor.getMajors();
+		ArrayList<ArrayList> myMinors = DBClassAccessor.getMinors();
 		
 		majorList.removeAllItems();
-		majorList.addItem("");
+		majorList.addItem("Select Major");
+		// Majors
+		for(ArrayList<String> tempAL : myMajors){
+			majorList.addItem(tempAL.get(0) + " (" + tempAL.get(1) + ")");
+		}
 		
 		minorList.removeAllItems();
 		minorList.addItem("Select Minor");
+		// Minors
+		for(ArrayList<String> tempAL : myMinors){
+			minorList.addItem(tempAL.get(0) + " (" + tempAL.get(1) + ")");
+		}
+		
+		majorList.setSelectedItem("Select Major");
+		minorList.setSelectedItem("Select Minor");
 	}
 	
 	public static void main(String[] args){
@@ -149,6 +105,74 @@ public class GuestGUI implements ActionListener{
 	}
 	
 	public void actionPerformed(ActionEvent ae){
-		
+		if(ae.getSource() == jbGenReport){
+			
+			String majValue = (majorList.getSelectedItem().toString()).substring(0,4);
+			String minValue = (minorList.getSelectedItem().toString()).substring(0,4);
+			
+			if((majorList.getSelectedItem().toString()).equals("Select Major")){
+				// JOptionPane Alert "You must select at least one major"
+			}else{
+				// Clear TextArea
+				reportTextArea.setText("");
+				
+				// Query DB, output to TextArea
+				ArrayList<ArrayList> tempMajReqAL = DBClassAccessor.getMajorRequirements(majValue);
+				
+				ArrayList<ArrayList> newSemesterBreakdown = new ArrayList<ArrayList>();
+				
+				String majorReportString = "---------------------------------------------------------------------------";
+				majorReportString = majorReportString + "\nMAJOR: " + majorList.getSelectedItem().toString() + "\n";
+				majorReportString = majorReportString + "---------------------------------------------------------------------------";
+				int count = 1;
+				int previousVal = 0;
+				
+				for(ArrayList<String> tempAL : tempMajReqAL){
+					try{
+						if(Integer.parseInt(tempAL.get(1)) > previousVal){	
+							// Output line
+							majorReportString = majorReportString + "\n-----------------------------Semester " + tempAL.get(1) + " -----------------------------";
+							previousVal = Integer.parseInt(tempAL.get(1));
+							
+							if(count % 2 == 0){
+								count--;
+							}
+						}
+					}catch(NumberFormatException nfe){
+						// If no semesters, this will be thrown - ignore
+					}
+					
+					if(count % 2 == 0){
+						majorReportString = majorReportString + "\t" + tempAL.get(0) + "\t(" + tempAL.get(1) + ")";
+					}else{
+						majorReportString = majorReportString + "\n" + tempAL.get(0) + "\t(" + tempAL.get(1) + ")";
+					}
+					count++;
+					
+				}
+
+				if(!((minorList.getSelectedItem().toString()).equals("Select Minor"))){
+					majorReportString = majorReportString + "\n---------------------------------------------------------------------------";
+					majorReportString = majorReportString + "\nMINOR: " + minorList.getSelectedItem().toString();
+					majorReportString = majorReportString + "\n---------------------------------------------------------------------------";
+					
+					ArrayList<String> tempMinReqAL = DBClassAccessor.getMinorRequirements(minValue);
+					// Append to reportTextArea
+					
+					count = 1;
+					
+					for(String tempS : tempMinReqAL){
+						if(count % 2 == 0){
+							majorReportString = majorReportString + "\t" + tempS + "\t";
+						}else{
+							majorReportString = majorReportString + "\n" + tempS + "\t";
+						}
+						count++;
+					}
+				}
+				
+				reportTextArea.setText(majorReportString);
+			}
+		}
 	}
 }
