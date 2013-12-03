ac5e1b3093ff63d234885da77f3ca145983249b1
Evan Reichard <evan@evanreichard.com>
2013-12-02 19:20:29 -0500

    JavaDoc and Student mySQL DB
    JavaDoc comments and started the interaction with the Student DB

--- AccessStudentData.java
+++ AccessStudentData.java
@@ -1,3 +1,12 @@
+/**
+ * Unlike the AccessCourseData class, this is a per-student object class.
+ * AccessCourseData is a more general object where you only have to use one
+ * to do most everything. 
+ *
+ * This class you need to define per student.
+ *
+ **/
+
 import java.sql.*;
 import java.util.ArrayList;
 
@@ -7,18 +16,69 @@ public class AccessStudentData{
 	private String majorID;
 	private String minorID;
 	
-	/*
-		Parameterized constructor AccessStudentData
-		@param String username - username of student to be accessed
-	*/
+	static Connection connection;
+	static Statement statement;
+	
+	/**
+	 * AccessStudentData Constructor
+	 * 
+	 * @param String username
+	 **/
 	public AccessStudentData(String username){
 		this.username = username;
 	}
-	/*
-		getClassIDs - Accesses the database to return an array of classIDs
-		@return String array of classIDs student has completed
+	
+	/**
+	 * executeQuery
+	 * 
+	 * This method is for opening and executing the query passed along to it. 
+	 * This method does not close the Connection that it opens, it must be closed 
+	 * via the methods that called it. This is because we return the ResultSet. 
+	 *
+	 * @param 	String		query
+	 * @return 	ResultSet 	rs
+	 **/
+	private ResultSet executeQuery(String query){
+		try {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            String database = "jdbc:odbc:Students";
+            
+			connection = DriverManager.getConnection(database,"",""); 
+			statement = connection.createStatement();
+			ResultSet rs = statement.executeQuery(query);
+			
+			// We return the ResultSet so the individual methods deal with the information.
+			// Don't forget! We need to close the connection once we're done in the method we return to.
+			return rs;
+			
+        }catch(Exception e){
+            e.printStackTrace();
+            System.out.println("Error!");
+        }
+		
+		return null;
+	}
+	
+	/**
+	 * getClassIDs - Accesses the database to return an array of classIDs
+	 *
+	 * @return String classIDs
 	*/
-	public String[] getClassIDs(){
+	public ArrayList<String> getClassIDs(){
+		String classIDQuery = "SELECT ClassIDs FROM Students WHERE username='" + username + "'";
+		ArrayList<String> classIDs = new ArrayList<String>();
+		
+		try{
+			String unparsedClassIDs = rs.getString(1)
+			
+			// Parse the classIDs (CSV)
+			
+			connection.close();
+		}catch(SQLException sqle){
+			sqle.printStackTrace();
+			System.out.println("Error!");
+		}
+		
 		return classIDs;
 	}
 	
@@ -28,17 +88,5 @@ public class AccessStudentData{
 	
 	public String getMinorID(){
 		return minorID;
-	}
-	
-	/*
-		getCredits - Accesses the database to return the number of credits 
-		student has completed thus far. 
-		
-		** This needs to be calculated in the Interface class:
-			- [AccessStudentData] Grab Students Completed Classes
-			- [AccessCourseData]  Query each class and return the total # of credits for each
-	*/
-	public int getCredits(){
-		return 1;
 	}
 }
